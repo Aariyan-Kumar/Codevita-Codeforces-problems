3d1f5895e16bf332c506aa7f0705f1085ba92907
Aariyan-Kumar <cst.21bctc85@silicon.ac.in>
2024-11-23 17:03:56 +0530

    justifyWords Renamed as Q41_JustifyWords

--- Q41_JustifyWords.java
+++ Q41_JustifyWords.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class JustifyWords{
+public class Q41_JustifyWords{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
