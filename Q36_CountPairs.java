34a3b2a5b2eb590c8ac966fc721416cf7c221a13
Aariyan-Kumar <cst.21bctc85@silicon.ac.in>
2024-11-25 20:02:04 +0530

    updated the edge case

--- Q36_CountPairs.java
+++ Q36_CountPairs.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class Q36_CountPairs {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -8,34 +9,32 @@ public class Q36_CountPairs {
 
         int A[] = new int[n];
 
-        for(int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             A[i] = sc.nextInt();
         }
 
-        System.out.println(solveCountPairs(A, k));
+        if (k != 0) {
+            System.out.println(solveCountPairs(A, k));
+        } else {
+            System.out.println(0);
+        }
 
         sc.close();
     }
 
-    private static int solveCountPairs(int[] a, int k) 
-    {
+    private static int solveCountPairs(int[] a, int k) {
         int count = 0;
 
-        for(int i = 0; i < a.length; i++)
-        {
+        for (int i = 0; i < a.length; i++) {
             int min = a[i] - k;
             int max = a[i] + k;
 
-            for(int j = 0; j < a.length; j++)
-            {
-                if(a[j] >= min && a[j] <= max && j != i)
-                {
+            for (int j = 0; j < a.length; j++) {
+                if (a[j] >= min && a[j] <= max && j != i) {
                     count++;
                     break;
                 }
             }
-
 
         }
 
